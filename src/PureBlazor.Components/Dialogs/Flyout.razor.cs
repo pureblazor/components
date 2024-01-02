a4f366c05b318db84927033f5c1f4a91583c363e
Cody Mullins <joshua.c.mullins@gmail.com>
2024-01-01 22:46:16 -0500

    initial flyout refactor

---
+++ src/PureBlazor.Components/Dialogs/Flyout.razor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace PureBlazor.Components.Dialogs;
+
+public partial class Flyout
+{
+    [Parameter]
+    public bool IsOpen { get; set; }
+
+    [Parameter]
+    public EventCallback OnDismiss { get; set; }
+}
