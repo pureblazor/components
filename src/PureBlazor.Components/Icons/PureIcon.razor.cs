f89e71491c7615f39cc10f8b64f75c6a9970af39
Marcus Kertesz <ModernMAK@Hotmail.com>
2024-01-11 12:29:22 -0800

    More Complex Pagination Component
    Used the requested resource for inspiration;
    
    Left out some features, since It's just a prototype

--- src/PureBlazor.Components/Icons/PureIcon.razor.cs
+++ src/PureBlazor.Components/Icons/PureIcon.razor.cs
@@ -26,6 +26,10 @@ public partial class PureIcon
         {
             PureIcons.IconCheck => "<path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"m4.5 12.75 6 6 9-13.5\" />",
             PureIcons.IconSpin => "<path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99\" />",
+            PureIcons.IconChevronDoubleLeft => "<path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"m18.75 4.5-7.5 7.5 7.5 7.5m-6-15L5.25 12l7.5 7.5\" />",
+            PureIcons.IconChevronLeft => "<path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M15.75 19.5 8.25 12l7.5-7.5\" />",
+            PureIcons.IconChevronDoubleRight => "<path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5\" />",
+            PureIcons.IconChevronRight => "<path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"m8.25 4.5 7.5 7.5-7.5 7.5\" />",
             _ => ""
         };
 
@@ -56,5 +60,9 @@ public partial class PureIcon
 public enum PureIcons
 {
     IconCheck,
-    IconSpin
+    IconSpin,
+    IconChevronDoubleLeft,
+    IconChevronLeft,
+    IconChevronDoubleRight,
+    IconChevronRight,
 }
---
+++ src/PureBlazor.Components/Pagination/PurePaginationItem.razor.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components;
+using PureBlazor.Components.Styling;
+using System.Drawing;
+
+namespace PureBlazor.Components.Pagination;
+
+public partial class PurePaginationItem
+{
+    [Parameter]
+    public int Value { get; set; }
+
+    [Parameter]
+    /// <summary>
+    /// Whether the current item should be rendered as the 'active' page.
+    /// </summary>
+    public bool Active { get; set; }
+
+    [Parameter]
+    /// <summary>
+    /// Disables the control; preventing it from being clicked.
+    /// </summary>
+    public bool Disabled { get; set; }
+
+    /// <summary>
+    /// Inner content for the Pagination Button.
+    /// </summary>
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+
+    [Parameter]
+    public EventCallback<int> OnClick { get; set; }
+    public async Task OnElementClick(MouseEventArgs args) => await OnClick.InvokeAsync(Value);
+    
+
+}
