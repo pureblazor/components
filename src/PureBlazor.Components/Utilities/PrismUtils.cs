ffef38fe9382c03f00e490b654ef3afeb19d1663
Luis Forero <lforero@hubb.me>
2024-01-16 14:54:09 -0600

    Update PrismUtils makani.js path

--- src/PureBlazor.Components/Utilities/PrismUtils.cs
+++ src/PureBlazor.Components/Utilities/PrismUtils.cs
@@ -9,7 +9,7 @@ public class PrismUtils
     public PrismUtils(IJSRuntime jsRuntime)
     {
         moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>(
-            "import", "./_content/Makani/makani.js").AsTask());
+            "import", "./_content/PureBlazor.Components/makani.js").AsTask());
     }
 
     public async ValueTask HighlightAll()
