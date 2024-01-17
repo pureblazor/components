c829bdd9d5417845f5d9736fa3b900eca49f1c0a
Luis Forero <lforero@hubb.me>
2024-01-16 20:54:37 -0600

    Comment /DarkModeEnabled = await ElementUtils.IsDarkMode(); on MkDarkModeToggle.razor Change MkComponent.razor to PureComponent.razor (remove MkComponent.razor) Update @inherits MkComponent to @inherits PureComponent

---
+++ src/PureBlazor.Components/Infrastructure/PureComponent.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Logging;
+
+namespace PureBlazor.Components;
+
+public partial class PureComponent
+{
+    [Inject]
+    protected ILogger<PureComponent> Log { get; set; }
+}
