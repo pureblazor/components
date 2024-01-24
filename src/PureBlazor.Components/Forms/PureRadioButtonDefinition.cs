16ea65df9235c3fa12ddbdf1f61a50e752817baa
Luis Forero <lforero@hubb.me>
2024-01-24 15:52:23 -0600

    Add initial radio button component

---
+++ src/PureBlazor.Components/Forms/PureRadioButtonDefinition.cs
@@ -0,0 +1,15 @@
+﻿namespace PureBlazor.Components.Forms
+{
+    public class PureRadioButtonDefinition
+    {
+        public string Name
+        {
+            //Remove spacial characeres?
+            //Remove spaces in between?
+            get { return Title.Trim(); }            
+        }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public bool IsChecked { get; set; } = false;
+    }
+}
