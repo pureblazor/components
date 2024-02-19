b284365895ff402231dc24c1b184547ddc9d02d1
Cody Mullins <joshua.c.mullins@gmail.com>
2024-02-19 08:07:38 -0500

    Update src/PureBlazor.Components/Utilities/NamingUtils.cs

--- src/PureBlazor.Components/Utilities/NamingUtils.cs
+++ src/PureBlazor.Components/Utilities/NamingUtils.cs
@@ -5,37 +5,39 @@ using System.Runtime.ExceptionServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PureBlazor.Components.Utilities
+namespace PureBlazor.Components.Utilities;
+public class NamingUtils
 {
-    public class NamingUtils
+    private readonly static char[] AcceptableCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+    private const int DefaultRandomLength = 8;
+    private static readonly Random randomGenerator = new Random();
+    private static readonly object mutexLock = new object();
+    private static readonly HashSet<string> previouslyUsedRandom = new HashSet<string>();
+    
+    public static string CreateRandomizedId()
     {
-        private readonly static char[] AcceptableCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
-        private const int DefaultRandomLength = 8;
-        private static readonly Random randomGenerator = new Random();
-        private static readonly object mutexLock = new object();
-        private static readonly HashSet<string> previouslyUsedRandom = new HashSet<string>();
-        public static string CreateRandomizedId()
+        return CreateRandomizedId(DefaultRandomLength);
+    }
+    
+    public static string CreateRandomizedId(int length)
+    {
+        if (length < 0)
         {
-            return CreateRandomizedId(DefaultRandomLength);
+            return "";
         }
-        public static string CreateRandomizedId(int length)
+        
+        lock (mutexLock)
         {
-            if(length < 0)
-            {
-                return "";
-            }
-            lock (mutexLock)
-            {
-                int startIndex = 0;
-                char[] randomizedChars = new char[length];
+            int startIndex = 0;
+            char[] randomizedChars = new char[length];
 
-                while(startIndex < length)
-                {
-                    randomizedChars[startIndex] = AcceptableCharacters[randomGenerator.Next(AcceptableCharacters.Length)];
-                    startIndex++;
-                }
-                return new String(randomizedChars);
+            while (startIndex < length)
+            {
+                randomizedChars[startIndex] = AcceptableCharacters[randomGenerator.Next(AcceptableCharacters.Length)];
+                startIndex++;
             }
+            
+            return new String(randomizedChars);
         }
     }
 }
