d8a5f5bfea7d8335eb3872fc651cc7b92782f8c4
Cody Mullins <joshua.c.mullins@gmail.com>
2024-05-03 14:23:02 -0400

    remove commented line

--- css.sh
+++ css.sh
@@ -4,5 +4,3 @@ source ~/.zshrc
 nvm use
 npm install tailwindcss@next @tailwindcss/vite@next
 npx @tailwindcss/cli@next -i src/Pure.Blazor.Components/tailwind.css -o src/Pure.Blazor.Components/wwwroot/pureblazor.css --watch
-
-# ./tailwindcss-macos-arm64 -c src/Pure.Blazor.Components/tailwind.config.js -i src/Pure.Blazor.Components/wwwroot/app.css -o src/Pure.Blazor.Components/wwwroot/pureblazor.css
