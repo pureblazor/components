0ca0ffb0df347c67f0c73767d817050375be0340
Cody Mullins <joshua.c.mullins@gmail.com>
2024-05-03 14:20:48 -0400

    Add IsPressed effect base

--- css.sh
+++ css.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 # file: css.sh
+source ~/.zshrc
+nvm use
+npm install tailwindcss@next @tailwindcss/vite@next
+npx @tailwindcss/cli@next -i src/Pure.Blazor.Components/tailwind.css -o src/Pure.Blazor.Components/wwwroot/pureblazor.css --watch
 
-./tailwindcss-macos-arm64 -c src/Pure.Blazor.Components/tailwind.config.js -i src/Pure.Blazor.Components/wwwroot/app.css -o src/Pure.Blazor.Components/wwwroot/pureblazor.css
+# ./tailwindcss-macos-arm64 -c src/Pure.Blazor.Components/tailwind.config.js -i src/Pure.Blazor.Components/wwwroot/app.css -o src/Pure.Blazor.Components/wwwroot/pureblazor.css
--- src/Pure.Blazor.Components/Buttons/PureButton.cs
+++ src/Pure.Blazor.Components/Buttons/PureButton.cs
@@ -6,11 +6,29 @@ using Pure.Blazor.Components.Primitives;
 
 namespace Pure.Blazor.Components.Buttons;
 
+public enum ButtonPressEffect
+{
+    None,
+
+    // what's the best name here?
+    Jiggle,
+
+    Ripple,
+}
+
 public class PureButton : PureButtonBase
 {
+    public bool IsPressed { get; set; }
+
     [Parameter] public PureIcons? LeftIcon { get; set; }
 
     [Parameter] public PureIcons? RightIcon { get; set; }
+
+    private void SetPressed(bool pressed)
+    {
+        Console.WriteLine($"Setting IsPressed to {pressed}");
+        IsPressed = pressed;
+    }
 
     protected override void BuildRenderTree(RenderTreeBuilder builder)
     {
@@ -24,7 +42,8 @@ public class PureButton : PureButtonBase
         }
 
         builder.AddAttribute(4, "onclick", EventCallback.Factory.Create<MouseEventArgs>(this, OnClicked));
-
+        builder.AddAttribute(5, "onmousedown", EventCallback.Factory.Create<MouseEventArgs>(this, () => SetPressed(true)));
+        builder.AddAttribute(6, "onmouseup", EventCallback.Factory.Create<MouseEventArgs>(this, () => SetPressed(false)));
         var buttonType = ButtonType switch
         {
             ButtonType.Submit => "submit",
@@ -32,20 +51,22 @@ public class PureButton : PureButtonBase
             _ => ""
         };
 
-        builder.AddAttributeIfNotNullOrEmpty(5, "type", buttonType);
-        builder.AddAttributeIfNotNullOrEmpty(6, "name", Name);
-        builder.AddAttributeIfNotNullOrEmpty(7, "title", Title);
-        builder.AddAttributeIfNotNullOrEmpty(8, "value", Value);
-        builder.AddAttributeIfNotNullOrEmpty(9, "class", ApplyStyle(InternalCss));
+        builder.AddAttributeIfNotNullOrEmpty(7, "type", buttonType);
+        builder.AddAttributeIfNotNullOrEmpty(8, "name", Name);
+        builder.AddAttributeIfNotNullOrEmpty(9, "title", Title);
+        builder.AddAttributeIfNotNullOrEmpty(10, "value", Value);
+
+        Console.WriteLine($"Rendering IsPressed: {IsPressed}");
+        builder.AddAttributeIfNotNullOrEmpty(11, "class", $"{ApplyStyle(InternalCss)} {(IsPressed ? "translate-y-px" : "translate-y-0")}");
         if (StopPropagation)
         {
-            builder.AddEventPreventDefaultAttribute(10, "onclick", true);
-            builder.AddEventStopPropagationAttribute(11, "onclick", true);
+            builder.AddEventPreventDefaultAttribute(12, "onclick", true);
+            builder.AddEventStopPropagationAttribute(13, "onclick", true);
         }
 
         if (LeftIcon is not null)
         {
-            builder.OpenRegion(12);
+            builder.OpenRegion(14);
             builder.OpenComponent<PureIcon>(1);
             builder.AddAttribute(2, "Icon", LeftIcon);
             builder.AddAttribute(3, "Size", Size);
@@ -55,20 +76,20 @@ public class PureButton : PureButtonBase
 
         if (LoadingText is not null && Loading)
         {
-            builder.AddContent(13, LoadingText);
+            builder.AddContent(15, LoadingText);
         }
         else if (ChildContent is not null)
         {
-            builder.AddContent(14, ChildContent);
+            builder.AddContent(16, ChildContent);
         }
         else
         {
-            builder.AddContent(15, Text);
+            builder.AddContent(17, Text);
         }
 
         if (RightIcon is not null)
         {
-            builder.OpenRegion(16);
+            builder.OpenRegion(18);
             builder.OpenComponent<PureIcon>(1);
             builder.AddAttribute(2, "Icon", RightIcon);
             builder.AddAttribute(3, "Size", Size);
